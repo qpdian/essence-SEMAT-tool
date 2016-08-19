397e0ab01a59894ff4638b6f899cdbc4958c8923
qpdian <qpdiam@gmail.com>
2016-08-19 00:42:54 -0500

    clean import not used

--- client/imports/alphaCards/card-form.component.ts
+++ client/imports/alphaCards/card-form.component.ts
@@ -1,29 +1,22 @@
-import { Component, OnInit, Directive, ElementRef, HostListener, Input} from '@angular/core';
-import { REACTIVE_FORM_DIRECTIVES, FORM_DIRECTIVES, FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Component, OnInit, Input} from '@angular/core';
+import { REACTIVE_FORM_DIRECTIVES, FormGroup, FormBuilder, Validators } from '@angular/forms';
 import template from './card-form.component.html';
 import { MD_CARD_DIRECTIVES } from '@angular2-material/card';
-import { MD_TOOLBAR_DIRECTIVES } from '@angular2-material/toolbar';
 import { MD_INPUT_DIRECTIVES } from '@angular2-material/input';
-import { MdCheckbox } from '@angular2-material/checkbox';
 import {MdIcon, MdIconRegistry} from '@angular2-material/icon';
-import { MD_LIST_DIRECTIVES } from '@angular2-material/list';
 import {HTTP_PROVIDERS} from '@angular/http';
-import { ROUTER_DIRECTIVES } from '@angular/router';
-import { HighlightDirective } from './highlight.directive';
 import {AlphaCard} from './alpha-card'
-import {CardDetailComponent} from './card-detail.component';
-import {CardService} from './card.service';
 @Component({
     selector: 'card-form',
     template,
-    directives: [HighlightDirective, REACTIVE_FORM_DIRECTIVES, MD_CARD_DIRECTIVES, MD_TOOLBAR_DIRECTIVES, MD_INPUT_DIRECTIVES, MdIcon, MD_LIST_DIRECTIVES, ROUTER_DIRECTIVES, MdCheckbox],
-    providers: [HTTP_PROVIDERS, MdIconRegistry, CardService],
+    directives: [REACTIVE_FORM_DIRECTIVES, MD_CARD_DIRECTIVES, MD_INPUT_DIRECTIVES, MdIcon],
+    providers: [HTTP_PROVIDERS, MdIconRegistry],
 })
 export class CardFormComponent implements OnInit {
     @Input()
     alphaCard: AlphaCard;
     addAlphaState: FormGroup;
-    constructor(private formBuilder: FormBuilder,private cardService: CardService) {
+    constructor(private formBuilder: FormBuilder) {
     }
     ngOnInit() {
         this.addAlphaState = this.formBuilder.group({
@@ -34,9 +27,7 @@ export class CardFormComponent implements OnInit {
         this.addAlphaState.controls['nameState']['updateValue']('');
     }
     addAlphaStateCard() {
-                  alert('fff');
         if (this.addAlphaState.valid) {
-  
             this.alphaCard.addAlphaState(this.addAlphaState.value.nameState);
             this.resetForm();
         }
--- client/imports/alphaCards/card.component.ts
+++ client/imports/alphaCards/card.component.ts
@@ -3,12 +3,13 @@ import template from './card.component.html';
 import { MD_CARD_DIRECTIVES } from '@angular2-material/card';
 import { MD_TOOLBAR_DIRECTIVES } from '@angular2-material/toolbar';
 import { MD_INPUT_DIRECTIVES } from '@angular2-material/input';
+import {MD_GRID_LIST_DIRECTIVES} from '@angular2-material/grid-list';
 import { MdCheckbox } from '@angular2-material/checkbox';
 import {MdIcon, MdIconRegistry} from '@angular2-material/icon';
 import { MD_LIST_DIRECTIVES } from '@angular2-material/list';
 import {HTTP_PROVIDERS} from '@angular/http';
 import { ROUTER_DIRECTIVES } from '@angular/router';
-import { HighlightDirective } from './highlight.directive';
+import { HighlightDirective } from '../common/highlight.directive';
 import {AlphaCard} from './alpha-card'
 import {CardDetailComponent} from './card-detail.component';
 import {CardFormComponent} from './card-form.component';
@@ -16,7 +17,7 @@ import {CardService} from './card.service';
 @Component({
     selector: 'card',
     template,
-    directives: [HighlightDirective, CardDetailComponent, CardFormComponent, MD_CARD_DIRECTIVES, MD_TOOLBAR_DIRECTIVES, MD_INPUT_DIRECTIVES, MdIcon, MD_LIST_DIRECTIVES, ROUTER_DIRECTIVES, MdCheckbox],
+    directives: [HighlightDirective, CardDetailComponent, CardFormComponent, MD_CARD_DIRECTIVES, MD_INPUT_DIRECTIVES, MdIcon, MD_LIST_DIRECTIVES, MD_GRID_LIST_DIRECTIVES,ROUTER_DIRECTIVES, MdCheckbox],
     providers: [HTTP_PROVIDERS, MdIconRegistry, CardService],
 })
 
@@ -25,13 +26,6 @@ export class CardComponent implements OnInit {
     cards: AlphaCard[] = [];
     selectedCard: AlphaCard;
     constructor(private cardService: CardService) {
-        // this.cards.push(new AlphaCard(1, 'Stakeholder', 'Customer'));
-        // this.cards.push(new AlphaCard(2, 'Oportunity', 'Customer'));
-        // this.cards.push(new AlphaCard(3, 'Requeriments', 'Solution'));
-        // this.cards.push(new AlphaCard(4, 'Software', 'Solution'));
-        // this.cards.push(new AlphaCard(5, 'Team', 'Endeavor'));
-        // this.cards.push(new AlphaCard(6, 'Way of working', 'Endeavor'));
-        // this.cards.push(new AlphaCard(7, 'Work', 'Endeavor'));
     }
     ngOnInit() {
         this.getCards();
